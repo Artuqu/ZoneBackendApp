4fb71401d9b8d436ecee724d4ecbc24a0d133576
Artuqu <percikartur@gmail.com>
2022-03-09 20:53:13 +0100

    Init commit

---
+++ src/main/java/pl/zone/ZoneBackendApp/ZoneBackendAppApplication.java
@@ -0,0 +1,13 @@
+package pl.zone.ZoneBackendApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoneBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoneBackendAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/zone/ZoneBackendApp/ZoneBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zone.ZoneBackendApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoneBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
